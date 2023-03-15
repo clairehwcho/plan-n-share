67e14dc97b6dd1715c49dd0302407e9dc50653cb
Claire <claire.hw.cho@gmail.com>
2023-03-15 01:52:31 -0700

    add check-overdue function to dashboard.js

--- flask_app/static/js/dashboard.js
+++ flask_app/static/js/dashboard.js
@@ -12,86 +12,85 @@ const render_today = () => {
 };
 
 const render_summary_nums = () => {
-    const userListTable = document.querySelector('#user-list-table');
-    const totalNumOfUserTasks = document.querySelector('#total-num-of-user-tasks');
-    const numOfUserTasksToDo = document.querySelector('#num-of-user-tasks-to-do');
-    const numOfUserTasksInProgress = document.querySelector('#num-of-user-tasks-in-progress');
-    const numOfUserTasksDone = document.querySelector('#num-of-user-tasks-done');
+    const userListTableEl = document.querySelector('#user-list-table');
+    const totalNumOfUserTasksEl = document.querySelector('#total-num-of-user-tasks');
+    const numOfUserTasksToDoEl = document.querySelector('#num-of-user-tasks-to-do');
+    const numOfUserTasksInProgressEl = document.querySelector('#num-of-user-tasks-in-progress');
+    const numOfUserTasksDoneEl = document.querySelector('#num-of-user-tasks-done');
+    const numOfUserOverdueTasksEl = document.querySelector('#num-of-user-overdue-tasks');
 
-    const teamListTable = document.querySelector('#team-list-table');
-    const totalNumOfTeamTasks = document.querySelector('#total-num-of-team-tasks');
-    const numOfTeamTasksToDo = document.querySelector('#num-of-team-tasks-to-do');
-    const numOfTeamTasksInProgress = document.querySelector('#num-of-team-tasks-in-progress');
-    const numOfTeamTasksDone = document.querySelector('#num-of-team-tasks-done');
-
-    let countUserTaskToDo = 0;
-    let countUserTaskInProgress = 0;
-    let countUserTaskDone = 0;
-
-    let countTeamTaskToDo = 0;
-    let countTeamTaskInProgress = 0;
-    let countTeamTaskDone = 0;
-
-    userListTable.querySelectorAll('tbody > tr > td')
-        .forEach(td => {
-            switch (td.innerText) {
-                case "To do":
-                    countUserTaskToDo++;
-                    break;
-                case "In progress":
-                    countUserTaskInProgress++;
-                    break;
-                case "Done":
-                    countUserTaskDone++;
-                    break;
-            }
-        })
-
-    teamListTable.querySelectorAll('tbody > tr > td')
-        .forEach(td => {
-            switch (td.innerText) {
-                case "To do":
-                    countTeamTaskToDo++;
-                    break;
-                case "In progress":
-                    countTeamTaskInProgress++;
-                    break;
-                case "Done":
-                    countTeamTaskDone++;
-                    break;
-            }
-        })
+    const teamListTableEl = document.querySelector('#team-list-table');
+    const totalNumOfTeamTasksEl = document.querySelector('#total-num-of-team-tasks');
+    const numOfTeamTasksToDoEl = document.querySelector('#num-of-team-tasks-to-do');
+    const numOfTeamTasksInProgressEl = document.querySelector('#num-of-team-tasks-in-progress');
+    const numOfTeamTasksDoneEl = document.querySelector('#num-of-team-tasks-done');
+    const numOfTeamOverdueTasksEl = document.querySelector('#num-of-team-overdue-tasks');
 
 
-    totalNumOfUserTasks.innerText = userListTable.querySelectorAll('tbody > tr').length;
-    numOfUserTasksToDo.innerText = countUserTaskToDo;
-    numOfUserTasksInProgress.innerText = countUserTaskInProgress;
-    numOfUserTasksDone.innerText = countUserTaskDone;
+    totalNumOfUserTasksEl.innerText = userListTableEl.querySelectorAll('tbody > tr').length;
+    numOfUserTasksToDoEl.innerText = userListTableEl.querySelectorAll(`[class*="to-do-icon"]`).length;
+    numOfUserTasksInProgressEl.innerText = userListTableEl.querySelectorAll(`[class*="in-progress-icon"]`).length;
+    numOfUserTasksDoneEl.innerText = userListTableEl.querySelectorAll(`[class*="done-icon"]`).length;
+    numOfUserOverdueTasksEl.innerText = userListTableEl.querySelectorAll(`[data-overdue*="true"]`).length;
 
-    totalNumOfTeamTasks.innerText = teamListTable.querySelectorAll('tbody > tr').length;
-    numOfTeamTasksToDo.innerText = countTeamTaskToDo;
-    numOfTeamTasksInProgress.innerText = countTeamTaskInProgress;
-    numOfTeamTasksDone.innerText = countTeamTaskDone;
-}
+    totalNumOfTeamTasksEl.innerText = teamListTableEl.querySelectorAll('tbody > tr').length;
+    numOfTeamTasksToDoEl.innerText = teamListTableEl.querySelectorAll(`[class*="to-do-icon"]`).length;
+    numOfTeamTasksInProgressEl.innerText = teamListTableEl.querySelectorAll(`[class*="in-progress-icon"]`).length;
+    numOfTeamTasksDoneEl.innerText = teamListTableEl.querySelectorAll(`[class*="done-icon"]`).length;
+    numOfTeamOverdueTasksEl.innerText = teamListTableEl.querySelectorAll(`[data-overdue*="true"]`).length;
+
+
+};
 
 const render_table_row_id = () => {
-    const userListTable = document.querySelector('#user-list-table');
-    const userListTableRows = userListTable.querySelectorAll('tbody > tr');
+    const userListTableEl = document.querySelector('#user-list-table');
+    const userListTableRows = userListTableEl.querySelectorAll('tbody > tr');
     userListTableRows.forEach((row, i) => {
         row.querySelector('.table-row-id').innerText = i + 1;
     })
 
-    const teamListTable = document.querySelector('#team-list-table');
-    const teamListTableRows = teamListTable.querySelectorAll('tbody > tr');
+    const teamListTableEl = document.querySelector('#team-list-table');
+    const teamListTableRows = teamListTableEl.querySelectorAll('tbody > tr');
     teamListTableRows.forEach((row, i) => {
         row.querySelector('.table-row-id').innerText = i + 1;
     })
-}
+};
+
+const render_status_icon = () => {
+    const statusTds = document.querySelectorAll(`[class*="status-td"]`);
+    statusTds.forEach(td => {
+        switch (td.innerText) {
+            case "To do":
+                td.querySelector('.status-icon').className = "to-do-icon";
+                break;
+            case "In progress":
+                td.querySelector('.status-icon').className = "in-progress-icon";
+                break;
+            case "Done":
+                td.querySelector('.status-icon').className = "done-icon";
+                break;
+        }
+    })
+};
+
+const check_overdue = () => {
+    const today = new Date()
+    const dueDateTds = document.querySelectorAll(`[class*="due-date-td"]`);
+    dueDateTds.forEach(td => {
+        const dueDate = new Date(td.innerText)
+        if (td.previousElementSibling.innerText !== "Done" && today.getTime() > dueDate.getTime()) {
+            td.style.color = "var(--highlight1)";
+            td.setAttribute("data-overdue", "true");
+        }
+    })
+};
 
 const confirm_delete = () => {
     return confirm("Are you sure you want to delete?");
 };
 
 render_today();
-render_summary_nums();
 render_table_row_id();
+render_status_icon();
+check_overdue();
+render_summary_nums();
