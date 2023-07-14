431559fa5d7277490427b692b33cde9b62f04691
Claire <claire.hw.cho@gmail.com>
2023-07-13 17:32:48 -0700

    turn off debug mode in server.py

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ from flask_app import app
 from flask_app.controllers import controller_routes, controller_users, controller_tasks, controller_teams
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
